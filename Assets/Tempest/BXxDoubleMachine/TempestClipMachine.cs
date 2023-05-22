cfb2d465583a7008121c9147ffb9cb8f6f57aa68
Tempest <rileytempest@gmail.com>
2023-05-21 20:24:17 -0400

    kdljgs

--- Assets/Tempest/BXxDoubleMachine/TempestClipMachine.cs
+++ Assets/Tempest/BXxDoubleMachine/TempestClipMachine.cs
@@ -1,3 +1,31 @@
-$HEADER$namespace $NAMESPACE$ {
-  public class $CLASS$ : UnityEngine.MonoBehaviour {$END$}
+using Tempest.Machines.Bx;
+using UnityEditor;
+using UnityEngine;
+
+delegate void ClipEvent();
+
+
+namespace Tempest.Machines.Bx
+{
+    public class TempestClipMachine : ClipMachine
+    {
+        //Properties
+        private LavenderStack Lavender;
+        private CanaryStack Canary;
+        private EmeraldStack Emerald;
+
+        public TroikaColours PointerColour;
+        
+        //Events
+        private ClipEvent ChangeClipPointerEvent;
+        private ClipEvent RotateElementEvent;
+        private ClipEvent EjectElementEvent;
+        
+        //MenuHelpers
+        [MenuItem("TempestMx/CastEvent - ChangeClipPointerEvent")]
+        public static void MenuHelp_RoundRobinPointer()
+        {
+            Debug.Log("Robin has been been here");
+        }
+    }
 }
--- Assets/Tempest/BXxDoubleMachine/test_doublemachine.cs
+++ Assets/Tempest/BXxDoubleMachine/test_doublemachine.cs
@@ -1,9 +1,133 @@
 using UnityEngine;
 
-namespace Tempest.BXxDoubleMachine
+
+
+/******Tempest - faceless to hunt exotic mxchines
+ *
+ * What sub type can the exotic stack collect?
+ * They must be mutable.
+ * Scale: what command style interface would be best when mutating the stack entry
+ *
+ * How are we mutating the stack/object?
+ * Perhaps the stack itself should simply have an interface to cast events
+ * ignore the contents of the entry all together
+ *
+ * TODO: 001: Abstract/super class for stack instances.
+ * what to call 'exotic stacks' ? 
+ * ClipStacks
+ * Decorate ClipStacks with EventInterfaces
+ * IClipEvents
+ * blahblah : IClipEvents
+ * ClipStacks --> ClipMachine
+ * Concrete Classes: LavenderStack --> ClipStack
+ * Concrete Classes: CanarieStack ---> ClipStack
+ * Concrete Classes: EmeraldStack --> ClipStack
+ * TempestClipMachine
+ *
+ * Tabula Rasa
+ *
+ *
+ * events: change ClipPointer, mutateElement_ejectPop, mutateElement_rotate
+ * 
+ ********/
+
+
+namespace Tempest.Machines.Bx
 {
-    public class test_doublemachine : MonoBehaviour
+    public class ClipMachine : MonoBehaviour
     {
         
+    }
+
+    public class ClipStack //: System.Collections.Generic.Stack
+    {
+        
+    }
+}
+
+namespace Tempest.Machines.Bx
+{
+    //Enum
+    public enum TroikaColours
+    {
+        Lavender = 0,
+        Canary = 1,
+        Emerald = 2
+    }
+    
+    //Interfaces
+    public interface IClipMachine
+    {
+        void ClipPointerCommand(TroikaColours _pointTo);
+    }
+    public interface IJukeActions
+    {
+        void Eject(); //Set element into 'tray', removing/splicing stack element
+        void Rotate(); //Exchange element with 'tray'
+    }
+    public interface ILavenderStack : IJukeActions
+    {
+        void ToggleBoo();
+    }
+    public interface ICanaryStack : IJukeActions
+    {
+        void ToggleHoo();
+    }
+    public interface IEmeraldStack : IJukeActions
+    {
+        void ToggleFoo();
+    }
+    
+    //ConcreteClasses
+    public class LavenderStack : ClipStack, ILavenderStack
+    {
+        public void Eject()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Rotate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ToggleBoo()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class CanaryStack : ClipStack, ICanaryStack
+    {
+        public void Eject()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Rotate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ToggleHoo()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    public class EmeraldStack : ClipStack, IEmeraldStack
+    {
+        public void Eject()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Rotate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ToggleFoo()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
